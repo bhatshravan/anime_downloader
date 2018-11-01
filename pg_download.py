20adef53fc22996c121bcbbc13dc0fc9b799bb04
shravan <shrvnbhat0@gmail.com>
2018-11-01 18:21:35 +0530

    Added more pys

--- pg_download.py
+++ pg_download.py
@@ -1,16 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
+import cfscrape
 
-r = requests.get("https://www.rapidvideo.com/e/FT5OBW9GFR&q=720o")
+r = requests.get("http://kissanime.ru/Anime/Fairy-Tail-2014")
+url="http://kissanime.ru/Anime/Fairy-Tail-2014"
 
 #print(r.content)
 
-soup = BeautifulSoup(r.content, 'html.parser')
-video_mir1 = soup.find_all('source')
+def mira():
 
-for data in video_mir1:
-	print("<a href='",data['src'],"'>",data['src'],"</a>   ",data['title'])
+	soup = BeautifulSoup(r.content, 'html.parser')
+	video_mir1 = soup.find_all('source')
 
-f = open("output.html","wb")
-f.write(r.content)
-f.close()
+	for data in video_mir1:
+		print("<a href='",data['src'],"'>",data['src'],"</a>   ",data['title'])
+
+	f = open("output.html","wb")
+	f.write(r.content)
+	f.close()
+
+def kiss():
+
+
+	scraper = cfscrape.create_scraper()
+	content1 = scraper.get(url).content
+	content = (content1).decode("utf-8")
+	
+	soup = BeautifulSoup(content, 'html.parser')
+	video_mir1 = soup.find_all('a')
+
+
+
+	
+	f = open("output.html","wb")
+	f.write(content1)
+	for data in video_mir1:
+		print(data['href'])
+		
+		#print("<a href='",data['href'],"'>",data['title'],"</a>   ",data['title'])
+		#f.write("<a href='",data['href'],"'>",data['title'],"</a>   ",data['title'],"\n")
+
+	
+	
+	f.close()
+
+if __name__ == '__main__':
+	kiss()
